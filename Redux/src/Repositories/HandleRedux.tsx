1d0a3d313131c98f0d187c087402772885a5bca1
Lê Văn Võ <vole543215@gmail.com>
2023-07-19 19:24:48 +0700

    Redux_basic
    CRUD+store

---
+++ Redux/src/Repositories/HandleRedux.tsx
@@ -0,0 +1,34 @@
+import { produce } from "immer";
+
+const initialState: { products: [], isLoading: boolean, error: string } = {
+    products: [],
+    isLoading: false,
+    error: ""
+}
+
+export const productReducer = (state = initialState as any, action: any) => {
+    return produce(state, draftState => {
+        switch (action.type) {
+            case "product/fetchProducts":
+                draftState.products = action.payload
+                break;
+            case "product/addProduct":
+                draftState.products.push(action.payload.data);
+                break;
+            case "product/updateProduct":
+                const product = action.payload.data
+                draftState.products = draftState.products.map((item: any) => item.id === product.id ? product : item)
+                // draftState.products[product.id] = product;
+                console.log(product);
+                
+                break;
+            case "product/deleteProduct":
+                const id = action.payload;
+                draftState.products = state.products.filter((item: any) => item.id !== id)
+                break;
+            default:
+                return state;
+        }
+    })
+
+}
